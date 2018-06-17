a820952ff4c8da356e7cb3cba8b217ce404297f3
seon0 <syjo.develop@gmail.com>
2018-06-17 15:34:34 +0900

    make LoginActivity

---
+++ app/src/main/java/com/example/seonyoung/my_application/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.example.seonyoung.my_application
+
+import android.graphics.Color
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+
+class LoginActivity : AppCompatActivity() {
+
+    var login :Button? = null
+    var signin :Button? = null
+    var mFirebaseRemoteConfig: FirebaseRemoteConfig? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+        var splash_background = mFirebaseRemoteConfig?.getString("splash_background")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(Color.parseColor(splash_background))
+        }
+
+
+        login = findViewById(R.id.loginActivity_button_login)
+        signin = findViewById(R.id.loginActivity_button_sign)
+        login?.setBackgroundColor(Color.parseColor(splash_background))
+        signin?.setBackgroundColor(Color.parseColor(splash_background))
+
+
+    }
+}
--- app/src/main/java/com/example/seonyoung/my_application/SplashActivity.kt
+++ app/src/main/java/com/example/seonyoung/my_application/SplashActivity.kt
@@ -1,11 +1,13 @@
 package com.example.seonyoung.my_application
 
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.WindowManager
 import android.widget.LinearLayout
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
@@ -19,6 +21,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         Log.d("hi", "hihi")
 
         linearLayout = findViewById(R.id.splashactivity_linearLayout)
@@ -27,10 +30,11 @@ class SplashActivity : AppCompatActivity() {
                 .setDeveloperModeEnabled(BuildConfig.DEBUG)
                 .build()
         mFirebaseRemoteConfig?.setConfigSettings(configSettings)
-        mFirebaseRemoteConfig?.setDefaults(R.xml.default_config);
+        mFirebaseRemoteConfig?.setDefaults(R.xml.default_config)
 
         mFirebaseRemoteConfig?.fetch(0)//시간(초)
                 ?.addOnCompleteListener(this, OnCompleteListener<Void> { task ->
+
                     if (task.isSuccessful) {
 
                         // After config data is successfully fetched, it must be activated before newly fetched
@@ -48,6 +52,7 @@ class SplashActivity : AppCompatActivity() {
         var caps = mFirebaseRemoteConfig?.getBoolean("splash_message_caps")
         var splash_message = mFirebaseRemoteConfig?.getString("splash_message")
 
+        Log.d("splash","${splash_background} , ${caps},  ${splash_message}")
         linearLayout?.setBackgroundColor(Color.parseColor(splash_background))
 
         if(caps!!){
@@ -55,6 +60,9 @@ class SplashActivity : AppCompatActivity() {
             builder.setMessage(splash_message).setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i -> finish()  })
 
             builder.create().show()
+        }
+        else {
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
         }
     }
 
